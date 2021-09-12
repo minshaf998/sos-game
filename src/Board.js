f06f9460d5dd8d769bb53565ee0b039140999b71
minshaf998 <minshaf998@gmail.com>
2021-09-13 01:40:17 +0530

    bug fixed

--- src/Board.js
+++ src/Board.js
@@ -81,7 +81,6 @@ class Game extends React.Component {
 
   render() {
     const squares = this.props.squares.slice();
-    console.log(squares);
     const indexes = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
     let result = "";
 
