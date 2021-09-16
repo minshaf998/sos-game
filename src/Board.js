82abe10af7d26bc05762fa9113988604a6fc2134
minshaf998 <minshaf998@gmail.com>
2021-09-16 16:23:40 +0530

    changed to functional components

--- src/Board.js
+++ src/Board.js
@@ -1,126 +1,107 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default class Board extends React.Component{
-  constructor(props) {
-      super(props);
-      this.state = {
-        squares: Array(9).fill(null),
-        isx: true  
-      };
-  }
-  
-  handleClick(i) {
-    const squares = this.state.squares.slice();
+function Board(props) {
 
-    if(this.state.squares[i] === null){
-      if(this.state.isx){
-        squares[i] = 'S';}
-    
-        else{
-            squares[i] = 'O';
-        }
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isx, setIsx] = useState(true);
 
-        this.setState({squares: squares,isx: !this.state.isx });
+  function handleClick(i) {
+
+    if(squares[i] === null){
+      if(isx){
+        squares[i] = 'S';
       }
-    }
+      else{
+       squares[i] = 'O';
+      }
 
-  
-  renderSquare(i) {
+      setSquares(squares);
+      setIsx(!isx);
+    }
+  }
+
+  function renderSquare(i) {
     return (
       <React.Fragment>
-
         <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)} 
-      />          
-      
+        value={squares[i]}
+        onClick={() => handleClick(i)} 
+      />
       </React.Fragment>
     );
   }
 
-        
-  render() {
-    let status = 'Next player: ';
-
-    status = this.state.isx === true ? status += 'S' : status += 'O';
-
-    return (
-      <React.Fragment>
+  let status = 'Next player: ';
+  status = isx === true ? status += 'S' : status += 'O';
+  
+  return (
+    <React.Fragment>
         <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
+          {renderSquare(0)}{renderSquare(1)}{renderSquare(2)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
+          {renderSquare(3)}{renderSquare(4)}{renderSquare(5)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
+          {renderSquare(6)}{renderSquare(7)}{renderSquare(8)}
         </div>
       </div>
-      <Game squares={this.state.squares}/>
+      <Game squares={squares}/>
       </React.Fragment>
-    );
-  }
+  );
 }
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+export default Board;
+
+function Square(props) {
+  return (
+    <button
+      className="square"
+      onClick={() => props.onClick()}
+    >
+      {props.value}
+    </button>
+  );
 }
 
-class Game extends React.Component {
+function Game(props) {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      result : ''
-    };
-  }
-
-  render() {
-    const squares = this.props.squares.slice();
-    const indexes = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
-
-    if(this.state.result === ''){
-      indexes.forEach(element => {
-        // let result = this.state.result;
-        if( squares[element[0]] === 'S' && squares[element[1]] === 'S' && squares[element[2]] === 'S'){
-          this.state.result = 'S Wins';
-          // this.setState({result:result})
-          
-        }
-        else if( squares[element[0]] === 'O' && squares[element[1]] === 'O' && squares[element[2]] === 'O'){
-          this.state.result = 'O Wins';
-          // this.setState({result:result})
-        }
-      });
-    }
-
-    if((squares[0] === 'S' || squares[0] === 'O') && this.state.result === ''){
-      let allnull = false;
-
-      for (var i=0; i<squares.length; i++){
-        if(squares[i] === null ){
-          allnull = true;
-          break;
-        }
+  const [result, setResult] = useState('');
+  const squares = props.squares.slice();
+  const indexes = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+  
+  if(result === ''){
+    indexes.forEach(element => {
+      if( squares[element[0]] === 'S' && squares[element[1]] === 'S' && squares[element[2]] === 'S'){
+        setResult('S Wins');          
       }
-      if(allnull === false && (this.state.result !== 'S Wins' || this.state.result !== 'O Wins')){
-        this.state.result = 'Draw';
-        // this.setState({result:"result"})
+      else if( squares[element[0]] === 'O' && squares[element[1]] === 'O' && squares[element[2]] === 'O'){
+        setResult('O Wins');
+      }
+    });
+  }
+    
+  if((squares[0] === 'S' || squares[0] === 'O') && result === ''){
+    let allnull = false;
+    
+    for (var i=0; i<squares.length; i++){
+      if(squares[i] === null ){
+        allnull = true;
+        break;
       }
     }
 
-
-    return <div><h1>{this.state.result}</h1></div>;
+    if(allnull === false && (result !== 'S Wins' || result !== 'O Wins')){
+      setResult('Draw');
+    }
   }
+    
+  return (
+    <div>
+    <h1>{result}</h1>
+  </div>
+  
+  );
 }
